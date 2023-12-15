5b545e53805058891b8636b21cef70d77fba7a99
Sibongile <sibongile.nhlema@gmail.com>
2023-12-14 23:16:48 -0800

    fixed wrong size condition(edge cases check)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swapped;
 
-	if (size == 0)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < (size - 1); i++)
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
