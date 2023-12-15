a6dac10f1eb7d84562ec187714aaafe435423997
Sibongile <sibongile.nhlema@gmail.com>
2023-12-15 20:08:13 +0000

    updated and fixed merge sort function

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -21,11 +21,11 @@ void merge_sort(int *array, size_t size)
 		return;
 
 	buffer = malloc(sizeof(int) * size);
-	if (buff == NULL)
+	if (buffer == NULL)
 		return;
 
-	merge_sort_recursive(array, buffer, 0, size - 1);
-	free(buff);
+	merge_sort_recursive(array, buffer, 0, size);
+	free(buffer);
 }
 
 /**
@@ -38,43 +38,18 @@ void merge_sort(int *array, size_t size)
  */
 void merge_sort_recursive(int *array, int *buffer, size_t front, size_t back)
 {
-	size_t mid, i;
+	size_t mid;
 
-	if (front < back)
+	if (back - front > 1)
 	{
 		mid = front + (back - front) / 2;
 
 		/* Sort first half of the array */
 		merge_sort_recursive(array, buffer, front, mid);
 		/* Sort second half of the array */
-		merge_sort_recursive(array, buffer, mid + 1, back);
+		merge_sort_recursive(array, buffer, mid, back);
 		/* Merge the sorted subarrays*/
-		printf("Merging... \n");
-		printf("[left]: ");
-		for (i = front; i <= mid; i++)
-		{
-			printf("%d", array[i]);
-			if (i != mid)
-				printf(", ");
-		}
-		printf("\n");
-		printf("[right]: ");
-		for (i = mid + 1; i <= back; i++)
-		{
-			printf("%d", array[i]);
-			if (i != back)
-				printf(", ");
-		}
-		printf("\n");
 		merge_arrays(array, buffer, front, mid, back);
-		printf("[Done]: ");
-		for (i = front; i <= back; i++)
-		{
-			printf("%d", array[i]);
-			if (i != back)
-				printf(", ");
-		}
-		printf("\n");
 	}
 }
 
@@ -90,41 +65,44 @@ void merge_sort_recursive(int *array, int *buffer, size_t front, size_t back)
 void merge_arrays(int *array, int *buffer,
 		size_t front, size_t mid, size_t back)
 {
-	size_t i, j, k;
+	size_t i, j, k = 0;
 
-	/* Copy subarrays into buffer */
-	for (i = front; i <= back; i++)
-		buffer[i] = array[i];
+	printf("Merging...\n[left]: ");
+	print_array(array + front, mid - front);
+	printf("[right]: ");
+	print_array(array + mid, back - mid);
+
 	i = front;
-	j = mid + 1;
-	k = front;
-	/* Merge subarrays in sorted order */
-	while (i <= mid && j <= back)
+	j = mid;
+	k = 0;
+	/* Merge the two subarrays into the buffer */
+	while (i < mid && j < back)
 	{
-		if (buffer[i] <= buffer[j])
+		if (array[i] < array[j])
 		{
-			array[k] = buffer[i];
+			buffer[k] = array[i];
 			i++;
 		}
 		else
 		{
-			array[k] = buffer[j];
+			buffer[k] = array[j];
 			j++;
 		}
 		k++;
 	}
-	/* Copy remaining elements from first subarray */
-	while (i <= mid)
-	{
-		array[k] = buffer[i];
-		i++;
-		k++;
-	}
-	/* Copy remaining elements from second subarray */
-	while (j <= back)
-	{
-		array[k] = buffer[j];
-		j++;
-		k++;
-	}
+	/* Copy remaining elements from left subarray to buffer */
+	while (i < mid)
+		buffer[k++] = array[i++];
+	/* Copy remaining elements from right subarray to buffer */
+	while (j < back)
+		buffer[k++] = array[j++];
+
+	i = front;
+	k = 0;
+	/* Copy merged elements from the buffer back to original array */
+	while (i < back)
+		array[i++] = buffer[k++];
+
+	printf("[Done]: ");
+	print_array(array + front, back - front);
 }
