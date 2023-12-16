e3b3a47416dfc538054fa80e0f0973eb35246657
Sibongile <sibongile.nhlema@gmail.com>
2023-12-16 12:22:11 +0000

    implemented radix sort using LSD and counting sort

--- sort.h
+++ sort.h
@@ -30,6 +30,7 @@ void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 /*Helper Functions*/
 void quick_sort_helper(int *array, int left_pointer, int right_pointer,
