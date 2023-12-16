31c1b481751affdf2aadc80fd7e3c0666fa5de6d
Sibongile <sibongile.nhlema@gmail.com>
2023-12-16 11:37:26 +0000

    started task 9

---
+++ 105-radix_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * radix_sort -  sorts an array of integers in ascending order
+ * using the LSD Radix sort algorithm
+ *
+ * @array: the array of integers
+ * @size: the size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+}
