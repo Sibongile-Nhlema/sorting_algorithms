3029410c55511551f190e0065177912140602234
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-17 14:02:35 +0200

    Add data structures

--- sort.h
+++ sort.h
@@ -18,6 +18,49 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * enum kind_e - Card type
+ *
+ * @SPADE: Spade
+ * @HEART: Heart
+ * @CLUB: Club
+ * @DIAMOND: Diamond
+*/
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
