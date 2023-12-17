dc90f178ed03a08acd1446deb86bca510b03e0c3
Sibongile <sibongile.nhlema@gmail.com>
2023-12-17 03:59:36 -0800

    added boilerplates for the last task

---
+++ deck.h
@@ -0,0 +1,42 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+#endif
