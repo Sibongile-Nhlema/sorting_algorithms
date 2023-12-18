7bbd08f4c5e400828e1697b6f29c70f337cd2459
Sibongile <sibongile.nhlema@gmail.com>
2023-12-18 10:04:44 +0000

    fixed the betty errors

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -2,6 +2,7 @@
 
 void insertion_sort(card_t **cards, int size);
 int compare_cards(const void *card1, const void *card2);
+int custom_strcmp(const char *str1, const char *str2);
 /**
  *  sort_deck - sorts a deck of cards
  *  @deck: double pointer the linked list containing the cards
@@ -38,22 +39,23 @@ void sort_deck(deck_node_t **deck)
  * compare_cards - compares the value and kind of each card against the next
  * @card1: first card
  * @card2: second card
- * Return: 
+ * Return: the difference betwween the two kinds
  */
 int compare_cards(const void *card1, const void *card2)
 {
 	int i, index1, index2;
 	const card_t *c1 = *(const card_t **)card1;
 	const card_t *c2 = *(const card_t **)card2;
-	const char *card_values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+	const char *card_values[] = {"Ace", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "Jack", "Queen", "King"};
 	/* Get the indices of the card values in the desired order*/
 	index1 = -1;
 	index2 = -1;
 	for (i = 0; i < 13; i++)
 	{
-		if (strcmp(c1->value, card_values[i]) == 0)
+		if (custom_strcmp(c1->value, card_values[i]) == 0)
 			index1 = i;
-		if (strcmp(c2->value, card_values[i]) == 0)
+		if (custom_strcmp(c2->value, card_values[i]) == 0)
 			index2 = i;
 	}
 	/* Compare the indices of the card values */
@@ -61,6 +63,22 @@ int compare_cards(const void *card1, const void *card2)
 		return (index1 - index2);
 
 	return (c1->kind - c2->kind);
+}
+
+/**
+ * custom_strcmp - compares two strings
+ * @str1: fisrt string
+ * @str2: second string
+ * Return:  an integer indicating the order of the strings
+ */
+int custom_strcmp(const char *str1, const char *str2)
+{
+	while (*str1 && (*str1 == *str2))
+	{
+		str1++;
+		str2++;
+	}
+	return (*(unsigned char *)str1 - *(unsigned char *)str2);
 }
 
 /**
--- deck.h
+++ deck.h
@@ -5,12 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * enum kind_e - Card's suit
+ *
+ * The card has one of the possible values:
+ * @SPADE: Black suit
+ * @HEART: Red suit
+ * @CLUB: Black suit (three leaf clover)
+ * @DIAMOND: Red suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -22,8 +31,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -35,12 +44,13 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 deck_node_t *init_deck(const card_t cards[52]);
 void print_deck(const deck_node_t *deck);
 void sort_deck(deck_node_t **deck);
+
 #endif
