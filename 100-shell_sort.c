49b0e5558d9434832aa2fce9f1e9a34ea08959b6
Sibongile <sibongile.nhlema@gmail.com>
2023-12-18 13:08:06 +0000

    started task 4- shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ *
+ * @array: pointer to the array of integers
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	
+}
